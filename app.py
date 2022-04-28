be186f345f704f283a0c795074bfb1cba59770a3
said ahamada <elmaha269@gmail.com>
2022-04-28 22:53:51 +0200

    migration code

--- app.py
+++ app.py
@@ -2,12 +2,13 @@
 # conda create -- myenv python=3.6
 # conda activate myenv
 
-from fastapi import FastAPI
-from pydantic import BaseModel
+#from fastapi import FastAPI
+#from pydantic import BaseModel
+from flask import Flask , jsonify 
 #import pickle
 import joblib as jb 
 import pandas as pd 
-from sklearn.linear_model import LogisticRegression
+
 
 
 app = FastAPI()
@@ -15,18 +16,19 @@ X_test = pd.read_csv('X_test.csv')
 loaded_model = jb.load('best_model.joblib', 'r')
 #with open('LGBmodele.pkl','rb') as file:
  # loaded_model = pickle.load(file)
-class Client(BaseModel):    # maper la réalité 
-  index : float   # ça peut être le nu du client  
+#class Client(BaseModel):    # maper la réalité 
+ # index : float   # ça peut être le nu du client  
 # uvicorn app:app --reload
-@app.post('/predict')
-async def predict_var(client : Client):
+@app.route('/predict', methods =["POST"] )
+def predict_var():
+  data = request.get_json() 
   #data = client.dict()
   #print(data)
-  #id_client = int(data['index'])
-  id_client = 2
+  id_client = int(data['index'])
+  #id_client = 2
   prediction = loaded_model.predict(X_test.iloc[[id_client]])
   probability = loaded_model.predict_proba(X_test.iloc[[id_client]]).max()
-  return {
+  return jsonify({
     'prediction': str(prediction[0]),
     'probability': probability
-    }  
+    } )
