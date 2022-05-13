9356b594d7473f255b0bcd7b77262bd3a3182b0c
SAID AHAMADA <89577633+elmahasaid@users.noreply.github.com>
2022-05-13 11:58:28 +0200

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -2,16 +2,18 @@ import pandas as pd
 import streamlit as st 
 import requests as req 
 import joblib 
+import numpy as np
 import streamlit.components.v1 as components
 X_test = pd.read_csv("X_test.csv")
 list_explain = joblib.load("list_exp.joblib")
+X_test['AGE'] = X_test['DAYS_BIRTH'] / -365
 #option = st.selectbox( " veuillez choisir un client", list(X_test.index))
 #st.write(str(option))
 #url = "http://elmaha269.pythonanywhere.com/predict"
 #data = {"index":option}
 #reponse = req.post(url=url, json = data)
 #st.write(reponse.text)
-# streamlit run dashboard.py
+# streamlit run dashboard.py st.latex ou st.markdown
 
 
 #from urllib.request import urlopen 
@@ -19,7 +21,7 @@ list_explain = joblib.load("list_exp.joblib")
 #X_test = pd.read_csv("X_test.csv")
 
 #affichage du tableau de board
-st.title('Dashborad Credit Scoring')
+st.title('Dashboard Credit Scoring')
 st.subheader("Prédictions de scoring client")
 list_id = list(X_test.index)
 id_client = st.text_input("Veuillez choisir un client",)
@@ -35,6 +37,8 @@ elif(int(id_client) in list_id):
 
     #APP_url = "http://elmaha269.pythonanywhere.com/predict" + id_client
     APP_url = "http://elmaha269.pythonanywhere.com/predict"
+    st.title('Information du client')
+    st.dataframe(X_test.loc[int(id_client)])
 
     with st.spinner("Chargement du score du client..."):
         APP_data = {"index":id_client}
@@ -43,26 +47,24 @@ elif(int(id_client) in list_id):
         #APP_data = json.load(json_url.read())
         
         prediction= int(json_reponse['prediction'])
-        probability = json_reponse['probability']
+        probability = json_reponse['probability'] 
         def etat_client(pred):
             switch = {
                 1: 'La prédiction du client par rapport au crédit est de ' "" + str(prediction)+ '(non accordé) :' +" " 'Client à risque  d\'une approximation de ' + str(round(probability*100))+'%' + " " 'de risque de défaut.',
                 0: 'La prédiction du client par rapport au crédit est de ' "" + str(prediction)+ '(accordé) :' +" " 'Client peu de risque  d\'une approximation de ' + str(round(100 - probability*100))+'%' + " " 'de risque de défaut.'
             }
             return switch.get(pred)
-        
 
     
-        #if prediction == 1:
-         #   etat_client = 'Client à risque'+ str(round(probability*100))
-        #else:
-         #   etat_client= "Client peu de risque" + " " + str(round(100 - probability*100))+ '%'
-
-        #message = 'La prédiction du client est de : **'+ str(prediction) + "," " " + etat_client + '** avec **' + str(round(probability*100)) +'%** de risque de défaut.'
         message = etat_client(prediction)
 
         st.markdown(message)
         components.html(list_explain[int(id_client)].as_html(),height = 500)
+        i_n = np.where(X_test.index== int(id_client))[0][0]
+        st.bar_chart(X_test.iloc[i_n:i_n+19][['AGE']])
+        st.bar_chart(X_test.iloc[i_n:i_n+19][['DAYS_EMPLOYED']])
+        st.bar_chart(X_test.iloc[i_n:i_n+19][['CC_AMT_BALANCE_MEAN']])
+        
         
 else: 
     st.write('Identifiant non reconnu')
